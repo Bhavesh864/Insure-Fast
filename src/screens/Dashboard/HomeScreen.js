3d9750e91dd12ed15b0eacaf6d9a824d97bb1666
madhavsewag04 <madhavsewag04@gmail.com>
2023-04-29 16:31:04 +0530

    firstChange

--- src/screens/Dashboard/HomeScreen.js
+++ src/screens/Dashboard/HomeScreen.js
@@ -1,118 +1,136 @@
-import React, { useEffect, useState } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
-import { AvailableInsuranceComponent, HomeHeader, HomeScreenBanner, HowInsuranceWorkComponent } from '../../components/home/HomeComponents'
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
+import {
+  AvailableInsuranceComponent,
+  HomeHeader,
+  HomeScreenBanner,
+  HowInsuranceWorkComponent,
+} from '../../components/home/HomeComponents';
 // import HomeHeader from '../../components/home/HomeHeader'
-import { colors } from '../../styles/colors'
-import { Center, flexRow, screenStyle, width } from '../../styles/CommonStyling'
-import { AppText, HeadingText } from '../../Utility/TextUtility'
-import { useDispatch } from 'react-redux'
-import { getUserProfileData } from '../../store/actions/UserAction'
-import { CustomSafeAreaView } from '../../styles/SafeAreaCustomView'
-import { createCustomerAction, getRecentSearchedQuote } from '../../store/actions/PolicyAction'
-import { navigate } from '../../routes/RootNavigation'
+import {colors} from '../../styles/colors';
+import {Center, flexRow, screenStyle, width} from '../../styles/CommonStyling';
+import {AppText, HeadingText} from '../../Utility/TextUtility';
+import {useDispatch} from 'react-redux';
+import {getUserProfileData} from '../../store/actions/UserAction';
+import {CustomSafeAreaView} from '../../styles/SafeAreaCustomView';
+import {
+  createCustomerAction,
+  getRecentSearchedQuote,
+} from '../../store/actions/PolicyAction';
+import {navigate} from '../../routes/RootNavigation';
 
+const HomeScreen = ({navigation}) => {
+  const dispatch = useDispatch();
+  const [recentQuote, setrecentQuote] = useState(null);
 
-const HomeScreen = ({ navigation }) => {
-    const dispatch = useDispatch();
-    const [recentQuote, setrecentQuote] = useState(null);
+  useEffect(() => {
+    dispatch(getUserProfileData());
+    navigation.addListener('focus', () => {
+      getRecentSearchedQuote().then(res => {
+        // console.log('hello', res?.data == []);
+        if (res?.status && res?.data) {
+          setrecentQuote(res?.data[0]?.customer?.motorInsurance || false);
+        }
+      });
+    });
+  }, []);
 
-    useEffect(() => {
-        dispatch(getUserProfileData());
-        navigation.addListener('focus', () => {
-            getRecentSearchedQuote().then(res => {
-                // console.log('hello', res?.data == []);
-                if (res?.status && res?.data) {
-                    setrecentQuote(res?.data[0]?.customer?.motorInsurance || false);
-                }
-            })
-        });
-    }, []);
-
-
-    return (
-        <View style={screenStyle}>
-            <CustomSafeAreaView >
-                <HomeHeader />
-                <View style={{ flex: 1 }}>
-                    <ScrollView style={{ flex: 1 }}>
-                        <HomeScreenBanner />
-                        <AvailableInsuranceComponent
-                        />
-                        {recentQuote &&
-                            <View>
-                                <HeadingText
-                                    text="Continue with your last search"
-                                    style={{ paddingHorizontal: 20, paddingTop: 20 }}
-                                />
-                                <TouchableOpacity activeOpacity={0.8} onPress={() => navigate("quotationSummary", { recentQuote })} style={styles.claimReqCont}>
-                                    <View>
-                                        <Image source={require("../../assets/images/other/flagFill.png")} style={styles.flagImg} />
-                                    </View>
-                                    <View>
-                                        <HeadingText
-                                            text={`${recentQuote?.vehicle_make} ${recentQuote?.vehicle_model} ${recentQuote?.vehicle_variant} (${recentQuote?.vehicle_mfg_yr})`}
-                                            size={13}
-                                        />
-                                        <AppText
-                                            text={`3 quotes |  ₹13,836 onwards`}
-                                            style={{ top: -1 }}
-                                            size={12}
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-                            </View>}
-                        <HowInsuranceWorkComponent />
-                    </ScrollView>
-                </View>
-            </CustomSafeAreaView>
+  return (
+    <View style={screenStyle}>
+      <CustomSafeAreaView>
+        <HomeHeader />
+        <View style={{flex: 1}}>
+          <ScrollView showsVerticalScrollIndicator={false} style={{flex: 1}}>
+            <HomeScreenBanner />
+            <AvailableInsuranceComponent />
+            {recentQuote && (
+              <View>
+                <HeadingText
+                  text="Continue with your last search"
+                  style={{paddingHorizontal: 20, paddingTop: 20}}
+                />
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  onPress={() => navigate('quotationSummary', {recentQuote})}
+                  style={styles.claimReqCont}>
+                  <View>
+                    <Image
+                      source={require('../../assets/images/other/flagFill.png')}
+                      style={styles.flagImg}
+                    />
+                  </View>
+                  <View>
+                    <HeadingText
+                      text={`${recentQuote?.vehicle_make} ${recentQuote?.vehicle_model} ${recentQuote?.vehicle_variant} (${recentQuote?.vehicle_mfg_yr})`}
+                      size={13}
+                    />
+                    <AppText
+                      text={`3 quotes |  ₹13,836 onwards`}
+                      style={{top: -1}}
+                      size={12}
+                    />
+                  </View>
+                </TouchableOpacity>
+              </View>
+            )}
+            <HowInsuranceWorkComponent />
+          </ScrollView>
         </View>
-    )
-}
-
-
+      </CustomSafeAreaView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 20,
-        borderColor: colors.grey,
-        ...flexRow
-    },
-    tabCont: {
-        flexDirection: "row",
-        height: 45,
-        margin: 20,
-        // width: "90%",
-        backgroundColor: colors.lightGrey,
-        borderRadius: 5
-    },
-    tabOption: {
-        flex: 1,
-        ...Center,
-        margin: 2,
-        borderRadius: 5
-    },
-    claimReqCont: {
-        height: 70,
-        marginHorizontal: 20,
-        backgroundColor: colors.lightBlue,
-        ...flexRow,
-        borderRadius: 15,
-        paddingHorizontal: 20,
-        marginVertical: 10
-    },
-    flagImg: {
-        height: 22,
-        width: 22,
-        resizeMode: 'contain',
-        transform: [{ rotate: "12deg" }],
-        marginRight: 15
-    },
-    spacer: {
-        height: 20,
-        width: width,
-        backgroundColor: colors.off_white,
-        marginVertical: 20
-    }
-})
+  item: {
+    padding: 20,
+    borderColor: colors.grey,
+    ...flexRow,
+  },
+  tabCont: {
+    flexDirection: 'row',
+    height: 45,
+    margin: 20,
+    // width: "90%",
+    backgroundColor: colors.lightGrey,
+    borderRadius: 5,
+  },
+  tabOption: {
+    flex: 1,
+    ...Center,
+    margin: 2,
+    borderRadius: 5,
+  },
+  claimReqCont: {
+    height: 70,
+    marginHorizontal: 20,
+    backgroundColor: colors.lightBlue,
+    ...flexRow,
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  flagImg: {
+    height: 22,
+    width: 22,
+    resizeMode: 'contain',
+    transform: [{rotate: '12deg'}],
+    marginRight: 15,
+  },
+  spacer: {
+    height: 20,
+    width: width,
+    backgroundColor: colors.off_white,
+    marginVertical: 20,
+  },
+});
 
-export default HomeScreen
+export default HomeScreen;
--- src/screens/Dashboard/PoliciesScreen.js
+++ src/screens/Dashboard/PoliciesScreen.js
@@ -1,71 +1,75 @@
-import React from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import { screenStyle } from '../../styles/CommonStyling'
-import { HomeHeader } from '../../components/home/HomeComponents'
-import { getPunchedPoliciesData } from '../../store/actions/PolicyAction'
-import { useEffect } from 'react'
-import PoliciesDetailsComponent from '../../components/insurance/PoliciesDetailsComponent'
-import { useState } from 'react'
-import { AppText, HeadingText } from '../../Utility/TextUtility'
-import { colors } from '../../styles/colors'
+import React from 'react';
+import {FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {screenStyle} from '../../styles/CommonStyling';
+import {HomeHeader} from '../../components/home/HomeComponents';
+import {getPunchedPoliciesData} from '../../store/actions/PolicyAction';
+import {useEffect} from 'react';
+import PoliciesDetailsComponent from '../../components/insurance/PoliciesDetailsComponent';
+import {useState} from 'react';
+import {AppText, HeadingText} from '../../Utility/TextUtility';
+import {colors} from '../../styles/colors';
 
+const PoliciesScreen = ({navigation}) => {
+  const [policiesList, setpoliciesList] = useState(null);
 
-const PoliciesScreen = ({ navigation }) => {
-    const [policiesList, setpoliciesList] = useState(null);
+  useEffect(() => {
+    // console.log('hello');
+    navigation.addListener('focus', () => {
+      getPunchedPoliciesData().then(res => {
+        console.log('object', JSON.stringify(res));
+        if (res?.status) {
+          setpoliciesList(res?.data);
+        } else {
+          setpoliciesList([]);
+        }
+      });
+    });
+  }, []);
 
-    useEffect(() => {
-        // console.log('hello');
-        navigation.addListener('focus', () => {
-            getPunchedPoliciesData().then(res => {
-                console.log('object', JSON.stringify(res))
-                if (res?.status) {
-                    setpoliciesList(res?.data);
-                } else {
-                    setpoliciesList([])
-                }
-            })
-        });
-    }, [])
+  if (policiesList && policiesList.length == 0) {
+    return (
+      <SafeAreaView style={screenStyle}>
+        <View style={{flex: 1}}>
+          <HomeHeader screen={'policy'} />
+          <View
+            style={{
+              position: 'absolute',
+              bottom: '40%',
+              alignSelf: 'center',
+              alignItems: 'center',
+            }}>
+            <HeadingText
+              text={'No policies yet'}
+              size={24}
+              // color={colors.darkGrey}
+            />
+            <AppText
+              text={"Let's figure out how we can still help you"}
+              color={colors.darkGrey}
+              style={{marginTop: 2}}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView style={screenStyle}>
+        <View style={screenStyle}>
+          <HomeHeader />
+          <FlatList
+            data={policiesList}
+            keyExtractor={(item, index) => index}
+            renderItem={(item, index) => {
+              return <PoliciesDetailsComponent item={item} />;
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+};
 
+const styles = StyleSheet.create({});
 
-    if (policiesList && policiesList.length == 0) {
-        return (
-            <View style={{ flex: 1 }}>
-                <HomeHeader screen={'policy'} />
-                <View style={{ position: "absolute", bottom: "40%", alignSelf: "center", alignItems: "center" }}>
-
-                    <HeadingText
-                        text={"No policies yet"}
-                        size={24}
-                    // color={colors.darkGrey}
-                    />
-                    <AppText
-                        text={"Let's figure out how we can still help you"}
-                        color={colors.darkGrey}
-                        style={{ marginTop: 2 }}
-                    />
-                </View>
-            </View>
-        )
-    } else {
-        return (
-
-            <View style={screenStyle}>
-                <HomeHeader />
-                <FlatList
-                    data={policiesList}
-                    keyExtractor={(item, index) => index}
-                    renderItem={(item, index) => {
-                        return <PoliciesDetailsComponent item={item} />
-                    }}
-                />
-
-            </View>
-        )
-    }
-}
-
-
-const styles = StyleSheet.create({})
-
-export default PoliciesScreen
+export default PoliciesScreen;
